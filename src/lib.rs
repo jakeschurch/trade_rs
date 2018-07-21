512581bc936db58ef3c92318dcfd6ef205c5dc4c
Jake Schurch <jakeschurch@gmail.com>
2018-07-20 20:37:28 -0400

    Feature/instruments (#1)
    Add instrument types
    
    * Add Currency traits
    
    * Repackage repo & add Order struct semantics
    
    * Try benchmarking
    
    * feat(instruments): Add Currency type
    
    - Add benchmarks
    - Add test suite
    
    * Implement Holding functionality
    
    - add Holding type
    - add tests for mod.rs
    - update dev dependencies
    - other minor changes
    
    Signed-off-by: Jake Schurch <jakeschurch@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
+extern crate chrono;
+extern crate num;
+
 #[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+#[macro_use]
+extern crate lazy_static;
+
+pub mod instruments;
